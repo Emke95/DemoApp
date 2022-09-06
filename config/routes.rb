1d0fd20532072e03f77f66d15d05a531b628a514
Emma Keyes <emmakeyes.1@hotmail.com>
2022-09-06 06:51:01 +0100

    Added global navbar with links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root'main#home'
-  et '/home', to: "main#home"
+  get '/home', to: "main#home"
   get '/about', to: "main#about"
   get '/contact', to: "main#contact"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
